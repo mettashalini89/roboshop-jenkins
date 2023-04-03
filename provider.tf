74e8ec3660ebec4a651507f3faaa6c20813dfdc4
mettashalini89@gmail.com <CatMatDog@123>
2023-04-03 12:58:04 -0600

    jenkins practice

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
 provider "jenkins" {
   server_url = "https://172.31.14.26:8080"
-  username   = "username"
-  password   = "password"
+  username   = data.aws_ssm_parameter.jenkins_user.value
+  password   = data.aws_ssm_parameter.jenkins_pass.value
+}
+
+data "aws_ssm_parameter" "jenkins_user"{
+  name = "jenkins.user"
+}
+
+data "aws_ssm_parameter" "jenkins_pass"{
+  name = "jenkins.pass"
 }
