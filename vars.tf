74b8a3f7d591d4dd489513bcf2e28ae2a8fbd0a8
mettashalini89@gmail.com <CatMatDog@123>
2023-04-03 20:44:05 -0600

    jenkins practice

--- vars.tf
+++ vars.tf
@@ -1,10 +1,16 @@
 variable "folders" {
-  default = ["Infra"]
+  default = ["Infra", "CI-pipeline"]
 }
 
 variable "jobs" {
   default = [
-    {name= "roboshop", folder= "Infra", repo_url= "https://github.com/mettashalini89/Roboshop_infra"}
+    {name= "roboshop", folder= "Infra", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
+    {name= "frontend", folder= "CI-pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
+    {name= "catalogue", folder= "CI-pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
+    {name= "cart", folder= "CI-pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
+    {name= "user", folder= "CI-pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
+    {name= "payment", folder= "CI-pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
+    {name= "shipping", folder= "CI-pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"}
 
   ]
 }
