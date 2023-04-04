d8951ba20765dfb13e32bc85cb66cf8874ff43f5
mettashalini89@gmail.com <CatMatDog@123>
2023-04-04 16:02:01 -0600

    jenkins practice

--- vars.tf
+++ vars.tf
@@ -5,12 +5,12 @@ variable "folders" {
 variable "jobs" {
   default = [
     {name= "roboshop", folder= "Infra", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
-    {name= "frontend", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
-    {name= "catalogue", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
-    {name= "cart", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
-    {name= "user", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
-    {name= "payment", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"},
-    {name= "shipping", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/Roboshop_infra"}
+    {name= "frontend", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/frontend"},
+    {name= "catalogue", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/catalogue"},
+    {name= "cart", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/cart"},
+    {name= "user", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/user"},
+    {name= "payment", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/payment"},
+    {name= "shipping", folder= "CI-Pipeline", repo_url= "https://github.com/mettashalini89/shipping"}
 
   ]
 }
